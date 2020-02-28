d4a8a166a59774543696c1247a6d8f67ebb9f532
alexbryw <alexbryw@gmail.com>
2020-02-28 14:01:49 +0100

    Added empty react components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import MainView from './MainView'
+
+function App() {
+  return (
+    <div>
+        <h1>Layout</h1>
+        <MainView />
+    </div>
+  );
+}
+
+export default App;
