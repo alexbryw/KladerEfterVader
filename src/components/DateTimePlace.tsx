9f6fad0a768f92da2ec736caf15e2e68118c89ec
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-02 19:50:52 +0100

    clock is ticking but gets error message

--- src/components/DateTimePlace.tsx
+++ src/components/DateTimePlace.tsx
@@ -1,18 +1,17 @@
 import React, {CSSProperties} from 'react';
 import moment from 'moment';
 import 'moment/locale/sv';
+import Clock from './Clock'
 
 
 export default function DateTimePlace() {
 
-  let currentTime = moment().format('LT');
+  //let currentTime = moment().format('LTS');
   let currentDate = moment().format('LL');
   
   return (
     <div style = {timeContainer}>
-      <div style = {timeItem}>
-        {currentTime}
-      </div>
+        <Clock />
       <div style = {timeItem}>
         {currentDate}
       </div>    
