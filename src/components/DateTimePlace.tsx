31ccf1d33bdcc2091633f2b11a4dcf4cebaa0ce4
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-02 18:16:05 +0100

    moment.js installed, time and date displayed

--- src/components/DateTimePlace.tsx
+++ src/components/DateTimePlace.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
+import moment from 'moment';
+import 'moment/locale/sv';
+
 
 export default function DateTimePlace() {
-  return (
-    <div>
 
+  let currentTime = moment().format('LT');
+  let currentDate = moment().format('LL');
+  
+  return (
+    <div style = {timeContainer}>
+      <div style = {timeItem}>
+        {currentTime}
+      </div>
+      <div style = {timeItem}>
+        {currentDate}
+      </div>    
     </div>
   );
+}
+
+const timeContainer: CSSProperties = {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const timeItem: CSSProperties = {
+  margin: '0 0 2rem 0'
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import DateTimePlace from './DateTimePlace'
 
 export default function Home() {
   return (
     <div>
       <h1>HOME!</h1>
+      <DateTimePlace />
     </div>
   );
 }
