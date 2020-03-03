f20caba3bb3fb5c5a69debc6674d553915027a15
Oskar Lindblad <lindbladoskar@gmail.com>
2020-03-03 14:27:00 +0100

    Working API...but wrong API

--- src/components/WeekOverview.tsx
+++ src/components/WeekOverview.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import * as React from 'react';
+import WeekDay from './WeekDay'
 
-export default function WeekOverview() {
-  return (
-    <div>
-      <h1>WEEK'S WEATHER!</h1>
-    </div>
-  );
+interface Props {
+  text: number
+}
+
+interface State {
+  weatherData?: WeatherData,
+  loading: boolean
+}
+
+interface WeatherData { //Stoppa in all data vi vill ha
+  name: string
+  age: number
+}
+
+export default class WeekOverview extends React.Component<Props, State>{
+  constructor(props:Props){
+    super(props);
+    this.state = { 
+      weatherData: undefined,
+      loading: false
+    };
+  }
+
+
+  async componentDidMount() {
+    this.setState({loading: true})
+    const response = await fetch("https://swapi.co/api/people/1")
+    const data = await response.json()
+    this.setState({
+      loading: false,
+      weatherData: data
+    })
+  }
+  
+  render(){
+    const kamel = this.state.loading ? "loading..." : this.state.weatherData?.name
+    const num = [1,2,3,4,5];
+      return (
+        <div>
+          {num.map((value) => <WeekDay key={value} text={value} />)}
+          {kamel}
+        </div>
+      );
+  }
 }
