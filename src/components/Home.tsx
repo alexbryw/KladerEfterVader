084d7b63718b43ad92d603146f44f940215c5ea2
alexbryw <alexbryw@gmail.com>
2020-03-03 22:47:20 +0100

    Show city temp, weather, wind from API in home.
    Weather data fetched from API in WeatherTemp component.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import WeatherTeamp from './WeatherTemp';
 
 export default function Home() {
   return (
     <div>
       <h1>HOME!</h1>
+      <WeatherTeamp />
     </div>
   );
 }
--- src/components/WeatherTemp.tsx
+++ src/components/WeatherTemp.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 
-export default function WeatherTeamp() {
-  return (
-    <div>
+interface Props{}
+interface State{
+  city: string
+  isLoaded: boolean
+  weather: any
+}
 
-    </div>
-  );
+export default class WeatherTeamp extends React.Component<Props, State> {
+  constructor(props: Props){
+    super(props)
+    this.state = {
+      city: "Göteborg",
+      isLoaded: false,
+      weather: null
+    }
+  }
+
+  async componentDidMount(){
+    this.setState({ isLoaded: false })
+    
+    const response =  await fetch("http://api.openweathermap.org/data/2.5/weather?q="
+    +this.state.city+"&appid=16da1da324d687a04c8aec0742e21c35")
+    
+    const data = await response.json()
+    // console.log("data under")
+    // console.log(data)   //Console to see what's inside API response.
+    this.setState({
+      weather: data,
+      isLoaded: true
+    })
+  }
+
+
+  render(){
+    if(!this.state.isLoaded){
+      return(
+        <div><h1>Loading...</h1></div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <h3>From WeatherTemp</h3>
+          <h2>{this.state.weather.name}</h2>
+          <h3>{(this.state.weather.main.temp - 273.15).toFixed(1)}°C)</h3>
+          <h3>{this.state.weather.weather[0].main}</h3>
+          <h3>{this.state.weather.weather[0].description}</h3>
+          <h3>{this.state.weather.wind.speed} m/s {this.state.weather.wind.deg}°</h3>
+        </div>
+      );
+    }
+  }
 }
