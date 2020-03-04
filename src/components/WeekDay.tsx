40e6c1ff22e6e2cb49c0ec10c0d06150c72fc7fc
Oskar Lindblad <lindbladoskar@gmail.com>
2020-03-04 20:02:41 +0100

    Collecting weeks weatherData and displays it

--- src/components/WeekDay.tsx
+++ src/components/WeekDay.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+
 interface Props {
-  text: number
+  weatherContent: any
 }
 
 export default class WeekDay extends React.Component<Props>{
@@ -9,10 +10,24 @@ export default class WeekDay extends React.Component<Props>{
       super(props);
       this.state={};
     }
+
+
     render() {
+      const imgURL = `http://openweathermap.org/img/wn/${this.props.weatherContent.weather[0].icon}@2x.png`;
+      const weekdayNameSE = ['Sön', 'Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör']
+      const weekdayNum = new Date(this.props.weatherContent.dt * 1000).getDay();
+      const weekdayName = weekdayNameSE[weekdayNum]
+
       return (
           <div>
-            <li>{this.props.text}</li>
+            <img width="40"
+              src={imgURL} 
+              alt={this.props.weatherContent.weather[0].description}/>
+            <p>
+              {weekdayName}{" - "}
+              {this.props.weatherContent.weather[0].description} {"  "}
+              {(this.props.weatherContent.main.temp - 273.15).toFixed(1)}°C
+              </p>
           </div>
       );
   }
