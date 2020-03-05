5b6e9142fb23fc46e4efb5f6d0f4f8b5faa015f1
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-05 22:36:02 +0100

    toggles buttontext

--- src/components/DayNightMode.tsx
+++ src/components/DayNightMode.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 
-export default function DayNightMode() {
-  return (
-    <div>
+interface Props{}
+interface State{
+  buttonText: string,
 
-    </div>
-  );
+}
+
+export default class DayNightMode extends React.Component <Props, State>{
+  constructor(props:Props){
+    super(props);
+    this.state = {
+      buttonText: 'Dag'
+    }
+    this.toggleDayNight = this.toggleDayNight.bind(this)
+  }
+
+  toggleDayNight(){
+    const newText = (this.state.buttonText === 'Dag'? 'Natt': 'Dag');
+    this.setState({buttonText:newText})
+  }
+
+  render(){
+    return (
+      <div style = {dayNightContainer}>
+        <button onClick = {this.toggleDayNight}> {this.state.buttonText} </button>
+      </div>
+    );
+  }
+}
+
+const dayNightContainer:CSSProperties = {
+  position: 'absolute',
+  top: '2%',
+  right: '2%',
+  zIndex: 2
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainView from './MainView'
 import Navbar from './Navbar'
+import DayNightMode from './DayNightMode'
 
 function App() {
   return (
     <div>
         <MainView />
+        <DayNightMode />
         <Navbar />
     </div>
   );
