1bb45cce1084d1415421b1eb5668249abc019eb9
Oskar Lindblad <lindbladoskar@gmail.com>
2020-03-05 15:42:38 +0100

    Displays 5-day forecast

--- src/components/WeekOverview.tsx
+++ src/components/WeekOverview.tsx
@@ -1,59 +1,79 @@
 import * as React from 'react';
 import WeekDay from './WeekDay'
 
+
 interface Props {
   weatherContent: any,
   key: number
 }
 
 interface State {
-  weatherDataMorning?: any,
-  weatherDataMidday?: any,
-  weatherDataEvening?: any,
+  weatherData?: any,
+  weatherToday?: any,
 }
 
 export default class WeekOverview extends React.Component<Props, State>{
   constructor(props:Props){
     super(props);
     this.state = { 
-      weatherDataMorning: undefined,
-      weatherDataMidday: undefined,
-      weatherDataEvening: undefined,
+      weatherData: undefined,
+      weatherToday: undefined,
     };
   }
-
 
   async componentDidMount() {
     const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?id=2673730&appid=16da1da324d687a04c8aec0742e21c35&lang=se");
     const data = await response.json();
-    const dataMorning = data.list.filter((reading:any) => reading.dt_txt.includes("09:00:00"));
-    const dataMidday = data.list.filter((reading:any) => reading.dt_txt.includes("12:00:00"));
-    const dataEvening = data.list.filter((reading:any) => reading.dt_txt.includes("18:00:00"));
-
-    // console.log(dataMorning);
-    //console.log(dataMidday);
-    // console.log(dataEvening);
-
+    const dataWeather = data.list.filter((reading:any) => reading.dt_txt.includes("12:00:00"));
     this.setState({
-      weatherDataMorning: dataMorning,
-      weatherDataMidday: dataMidday,
-      weatherDataEvening: dataEvening,
+      weatherData: dataWeather,
+    })
+    const responseToday  = await fetch("http://api.openweathermap.org/data/2.5/weather?id=2673730&appid=16da1da324d687a04c8aec0742e21c35&lang=se");
+    const dataWeatherToday = await responseToday.json();
+    this.setState({
+      weatherToday: dataWeatherToday,
     })
   }
+
+
   
   render() {
-    const weatherList = this.state.weatherDataMidday;
+    const weatherList = this.state.weatherData;
+    const weatherListToday = this.state.weatherToday;
+    const hour = new Date().getHours()
 
     if (!weatherList) {
-      return <li>Loading...</li>;
+      return <p>Loading...</p>;
     }
-    console.log(weatherList);
+    if (!weatherListToday) {
+      return <p>Loading...</p>;
+    }
+
+    if(hour > 12){
+      weatherList.pop();
+      return (
+        <div style={{ ...weatherListContainer}}>
+            <WeekDay weatherContent={weatherListToday} key="0" />
+            {
+            weatherList.map((weatherContent:any, index:number) => 
+            <WeekDay weatherContent={weatherContent} key={index} 
+            />)}
+        </div>
+      );
+    }
+
     return (
-      <div>
-        {weatherList.map((weatherContent:any, index:number) => <WeekDay weatherContent={weatherContent} key={index} />)}
+      <div style={{ ...weatherListContainer}}>
+          {weatherList.map((weatherContent:any, index:number) => 
+          <WeekDay weatherContent={weatherContent} key={index} 
+      />)}
       </div>
     );
   }
 }
 
 
+const weatherListContainer: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column'
+}
