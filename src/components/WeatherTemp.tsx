06736a2d8304ab67503e19450b0e7ea6659d89cc
alexbryw <alexbryw@gmail.com>
2020-03-06 10:12:01 +0100

    Added icon and feels like temp

--- src/components/WeatherTemp.tsx
+++ src/components/WeatherTemp.tsx
@@ -26,8 +26,8 @@ export default class WeatherTeamp extends React.Component<Props, State> {
     +this.state.city+"&lang="+this.state.language+"&appid=16da1da324d687a04c8aec0742e21c35")
     
     const data = await response.json()
-    // console.log("data under")
-    // console.log(data)   //Console to see what's inside API response.
+    console.log("data under")
+    console.log(data)   //Console to see what's inside API response.
     this.setState({
       weather: data,
       isLoaded: true
@@ -36,21 +36,31 @@ export default class WeatherTeamp extends React.Component<Props, State> {
 
 
   render(){
+
+
     if(!this.state.isLoaded){
       return(
-        <div><h1>Loading...</h1></div>
+        <div>
+          <h1>Loading... </h1>
+          <h1>WeatherTemp</h1>
+        </div>
       )
     }
     else {
+    const weatherIconUrl = "http://openweathermap.org/img/wn/"+ this.state.weather.weather[0].icon +"@2x.png";
+    const weatherIconALtDescription = "an icon of " + this.state.weather.weather[0].description;
+    const tempInCelsius =  (this.state.weather.main.temp - 273.15).toFixed(1);
+    const tempFeelsLikeC =  (this.state.weather.main.feels_like - 273.15).toFixed(1);
       return (
-        <div>
-          <h3>From WeatherTemp</h3>
+        <div className="WeatherTemp">
           {/* <h2>{this.state.weather.name}</h2> */}
           <h2>{this.state.city}</h2>
-          <h3>{(this.state.weather.main.temp - 273.15).toFixed(1)}°C)</h3>
-          <h3>{this.state.weather.weather[0].main}</h3>
+          <h3>Temp: {tempInCelsius}°C </h3>
+          <h3>Känns som {tempFeelsLikeC}</h3>
+          <img src={weatherIconUrl} alt={weatherIconALtDescription}></img>
           <h3>{this.state.weather.weather[0].description}</h3>
-          <h3>{this.state.weather.wind.speed} m/s {this.state.weather.wind.deg}°</h3>
+          <h3>Vind {this.state.weather.wind.speed} m/s </h3>
+          <h3>Riktning {this.state.weather.wind.deg}°</h3>
         </div>
       );
     }
